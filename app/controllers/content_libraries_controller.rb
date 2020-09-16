578ecb7396ce8f3f7fe47ee2e6eeb3dac852d109
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2020-09-15 21:44:17 -0400

    Created the routes and the views for the Content Library model

--- app/controllers/content_libraries_controller.rb
+++ app/controllers/content_libraries_controller.rb
@@ -1,2 +1,17 @@
 class ContentLibrariesController < ApplicationController
+  def index
+  end
+  def show
+  end
+  def new
+  end
+  def create
+  end
+
+  private
+
+  def content_libraries_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
+
 end
