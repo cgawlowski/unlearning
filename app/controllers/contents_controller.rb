1c853cb7e970fa16c8b9bc9ba5686b562019251a
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2020-09-22 21:56:54 -0400

    Finished the searched button - simple version

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,16 +1,10 @@
 class ContentsController < ApplicationController
   def index
-    # if params[:query].present?
-    #     @contents = Content.where(format: params[:video])
-    # else
+    if params[:search]=="true"
+      #binding.pry
+         @contents = Content.where(format: params[:format])
+    else
       @contents = Content.all
-  #  end
+    end
   end
-
-  private
-
-
-  # def random
-  #   display random content
-  # end
 end
