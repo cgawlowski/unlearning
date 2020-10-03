62549ffa12fbfcdabacbd57f84ad300192019b9e
Darina Kukarina <kukarina.darina42@gmail.com>
2020-10-03 15:14:27 -0400

    Add seeds, add new function, improve search

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,25 +1,7 @@
 class ContentsController < ApplicationController
 
   def index
-    @contents = []
-    if params[:search] == "true"
-      @contents_temp = Content.where(format: set_format)
-      if params[:category]
-        @contents_temp = @contents_temp.where(category: set_topic)
-      end
-      @contents_temp.each do |item|
-        if params[:duration] == "1" && item.duration < 5
-          @contents.push(item)
-        elsif params[:duration] == "2" && item.duration < 20
-          @contents.push(item)
-        elsif params[:duration] == "3" && item.duration > 20
-          @contents.push(item)
-        else @contents = Content.all
-        end
-      end
-    else
-      @contents = Content.all
-    end
+    find_contents
   end
 
   # def index
@@ -37,26 +19,6 @@ class ContentsController < ApplicationController
   # end
 
 
-  def index
-    @contents = []
-    if params[:search] == "true"
-      @contents_temp = Content.where(format: set_format)
-      @contents_temp.each do |item|
-        if params[:duration] == "1" && item.duration < 5
-          @contents.push(item)
-        elsif params[:duration] == "2" && item.duration < 20
-          @contents.push(item)
-        elsif params[:duration] == "3" && item.duration > 20
-          @contents.push(item)
-        else @contents = Content.all
-        end
-      end
-    else
-      @contents = Content.all
-    end
-end
-
-
   def set_time
     if params[:duration] == "1"
       return Content.where(:duration <=5)
@@ -72,10 +34,7 @@ end
   end
 
   def advanced_search
-    response = HTTParty.get(
-            'https://listen-api.listennotes.com/api/v2/search?q=star%20wars&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30&genre_ids=68%2C82&published_before=1580172454000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=0',
-            { headers: {"X-ListenAPI-Key" => ENV["LISTEN_API_KEY"]}})
-    @podcasts = JSON.parse(response.body)["results"]
+    find_contents
   end
 
 
@@ -103,4 +62,25 @@ end
     end
   end
 
+  def find_contents
+    @contents = []
+    if params[:search] == "true"
+      @contents_temp = Content.where(format: set_format)
+      if params[:category]
+        @contents_temp = @contents_temp.where(category: set_topic)
+      end
+      @contents_temp.each do |item|
+        if params[:duration] == "1" && item.duration < 5
+          @contents.push(item)
+        elsif params[:duration] == "2" && item.duration < 20
+          @contents.push(item)
+        elsif params[:duration] == "3" && item.duration > 20
+          @contents.push(item)
+        else "Oops, no results found"
+        end
+      end
+    else
+      @contents = Content.all
+    end
+  end
 end
