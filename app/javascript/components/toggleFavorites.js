c305cf0264f8c7ddc89230507d4b4430ce888e82
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2020-12-04 22:32:59 -0500

    Tried to create page transition and dynamic navbar (failure)

--- app/javascript/components/toggleFavorites.js
+++ app/javascript/components/toggleFavorites.js
@@ -1,3 +1,5 @@
+// Favorites button
+
 const toggleFavorite = (buttonHTML) => {
   // the content id is stored in the data attirbute in the HTML; check the index.html for content
   const contentId = buttonHTML.dataset.contentId;
@@ -29,3 +31,62 @@ export const toggleFavorites = () => {
     })
   }
 }
+
+// ---------Responsive-navbar-active-animation-----------
+
+function test(){
+  var tabsNewAnim = $('#navbarSupportedContent');
+  var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
+  var activeItemNewAnim = tabsNewAnim.find('.active');
+  var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+  var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+  var itemPosNewAnimTop = activeItemNewAnim.position();
+  var itemPosNewAnimLeft = activeItemNewAnim.position();
+  $(".hori-selector").css({
+    "top":itemPosNewAnimTop.top + "px", 
+    "left":itemPosNewAnimLeft.left + "px",
+    "height": activeWidthNewAnimHeight + "px",
+    "width": activeWidthNewAnimWidth + "px"
+  });
+  $("#navbarSupportedContent").on("click","li",function(e){
+    $('#navbarSupportedContent ul li').removeClass("active");
+    $(this).addClass('active');
+    var activeWidthNewAnimHeight = $(this).innerHeight();
+    var activeWidthNewAnimWidth = $(this).innerWidth();
+    var itemPosNewAnimTop = $(this).position();
+    var itemPosNewAnimLeft = $(this).position();
+    $(".hori-selector").css({
+      "top":itemPosNewAnimTop.top + "px", 
+      "left":itemPosNewAnimLeft.left + "px",
+      "height": activeWidthNewAnimHeight + "px",
+      "width": activeWidthNewAnimWidth + "px"
+    });
+  });
+}
+$(document).ready(function(){
+  setTimeout(function(){ test(); });
+});
+$(window).on('resize', function(){
+  setTimeout(function(){ test(); }, 500);
+});
+$(".navbar-toggler").click(function(){
+  setTimeout(function(){ test(); });
+});
+
+
+// Page transition //
+
+// Get the Sequence element
+var sequenceElement = document.getElementById("sequence");
+
+// See: http://sequencejs.com/documentation/#options
+var options = {
+  animateCanvas: false,
+  phaseThreshold: false,
+  preloader: true,
+  fadeStepWhenSkipped: true,
+  reverseWhenNavigatingBackwards: true
+}
+
+var mySequence = sequence(sequenceElement, options);
+
