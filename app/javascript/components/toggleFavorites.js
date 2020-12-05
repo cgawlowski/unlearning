ee097b0413ffb537d815028fbf27a3bce3d58315
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2020-12-05 12:24:48 -0500

    Cleaned the CSS, removed the navbar and transition experiments

--- app/javascript/components/toggleFavorites.js
+++ app/javascript/components/toggleFavorites.js
@@ -76,17 +76,3 @@ $(".navbar-toggler").click(function(){
 
 // Page transition //
 
-// Get the Sequence element
-var sequenceElement = document.getElementById("sequence");
-
-// See: http://sequencejs.com/documentation/#options
-var options = {
-  animateCanvas: false,
-  phaseThreshold: false,
-  preloader: true,
-  fadeStepWhenSkipped: true,
-  reverseWhenNavigatingBackwards: true
-}
-
-var mySequence = sequence(sequenceElement, options);
-
